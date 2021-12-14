effb688f6f6f3094605df646310ffd804e11097f
Ilolo Izu <iloloizu97@gmail.com>
2021-12-13 22:55:32 -0600

    commit tree 7893f95a941911d8e639d14f01d895f04bc04ceb

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+// let wrapAdjective = function (adjective)) {
+//     if (adjective == "a hard worker") {
+//     return function(adjective) {
+//         return `You are a *${adjective}!`
+//     }
+// } else if (adjective == "a dedicated programmer") {
+//     return `You are a ||${adjective}!`
+// }
+// }
+
+let wrapAdjective = function (style = "*") {
+    return function (adjective = "special") {
+        return `You are ${style}${adjective}${style}!`
+    }
+}
